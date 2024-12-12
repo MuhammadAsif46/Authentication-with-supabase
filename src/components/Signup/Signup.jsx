da87af03e9b4af8bd7dd19c3c004be1c13a02d9a
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-12 18:51:46 +0500

    add Signup Ui complete

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
-import { supabase } from "../../supabase/supabase";
+import { Link, useNavigate } from "react-router-dom";
 
-const signup = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  
-//   Signup function
-  const signupHandler = async () => {
+const Signup = () => {
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState("");
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  // Signup
+  const signupHandler = async (e) => {
+    e.preventDefault();
     try {
       const { data, error } = await supabase.auth.signUp({
         email: email,
@@ -19,9 +24,131 @@ const signup = () => {
       }
     } catch (error) {
       console.log("Error signing up user -->", error);
+      setErrorMessage(error.code);
     }
   };
-  return <div>signup</div>;
+
+  return (
+    <>
+      <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="text-center text-2xl font-bold">Sign up</div>
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            {errorMessage && (
+              <div className="text-red-500 text-center mb-5 cursor-pointer">
+                {errorMessage}
+              </div>
+            )}
+            <form
+              className="space-y-6"
+              action="#"
+              method="POST"
+              onSubmit={signupHandler}
+            >
+              {/* <div>
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  UserName
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="username"
+                    name="username"
+                    type="username"
+                    autoComplete="username"
+                    required
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    onChange={(e) => {
+                      setUser({
+                        ...user,
+                        userName: e.target.value,
+                      });
+                    }}
+                  />
+                </div>
+              </div> */}
+
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700 text-start"
+                >
+                  Email address
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    required
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    onChange={(e) => {
+                      setUser({
+                        ...user,
+                        email: e.target.value,
+                      });
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700 text-start"
+                >
+                  Password
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    minLength={8}
+                    maxLength={20}
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    onChange={(e) => {
+                      setUser({
+                        ...user,
+                        password: e.target.value,
+                      });
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="flex items-center justify-between">
+                <div className="text-sm">
+                  <span className="font-medium text-gray-700">
+                    Already have an Account ?{" "}
+                    <Link
+                      to="/login"
+                      className="text-indigo-600 hover:text-indigo-500"
+                    >
+                      Login
+                    </Link>
+                  </span>
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Sign up
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default signup;
+export default Signup;
